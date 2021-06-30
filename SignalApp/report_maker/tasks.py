7dced5651debc110fd01cab906ba60527f4f9117
VirusEnabled <graphs02@gmail.com>
2021-06-30 07:47:41 -0400

    adding changes based on feedback

--- SignalApp/report_maker/tasks.py
+++ SignalApp/report_maker/tasks.py
@@ -25,6 +25,38 @@ def load_stock_data_to_db():
         raise Exception(result['error'])
 
     else:
+        transaction_details = calculate_transactions_for_symbols(symbols=stock_list,
+                                                                 start_date=result['start_date'])
+        if not transaction_details['status']:
+            logger.error(f"{transaction_details['error']}")
+            raise Exception(transaction_details['error'])
+
+        else:
+            logger.info(f"The task was performed successfully.")
+            return f"The task was performed successfully " \
+                   f"for the symbols: {stock_list}, starting at{result['start_date']}"
+
+
+@shared_task
+def generate_transactions_for_existing_data():
+    """
+    generates the transactions for the
+    stocks based on the existing start_date
+    :return: None
+    """
+    stock_list = [symbol.symbol for symbol in Stock.objects.filter(priority=Stock.choices[0]).order_by('symbol')]
+    times = generate_time_intervals_for_api_query()
+
+    if not times['status']:
+        logger.error(f"{times['error']}")
+        raise Exception(times['error'])
+    result = calculate_transactions_for_symbols(symbols=stock_list,
+                                                start_date=times['start_date'])
+    if not result['status']:
+        logger.error(f"{result['error']}")
+        raise Exception(result['error'])
+
+    else:
         logger.info(f"The task was performed successfully.")
-
-
+        return f"The task was performed successfully " \
+               f"for the symbols: {stock_list}, starting at{result['start_date']}"
